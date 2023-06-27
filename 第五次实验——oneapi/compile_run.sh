926e7a9d90732b5a5d14c3a2f3c019791bbaf4e6
BianLianYaoGuai <99109657+BianLianYaoGuai@users.noreply.github.com>
2023-06-27 15:51:30 +0800

    Update compile_run.sh

--- 第五次实验——oneapi/compile_run.sh
+++ 第五次实验——oneapi/compile_run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source /opt/intel/oneapi/setvars.sh > /dev/null 2>&1
 /bin/echo "##" $(whoami) is compiling
-icpx -fsycl src/LU.cpp
+icpx -fsycl LU.cpp
 if [ $? -eq 0 ]; then ./a.out; fi
 
